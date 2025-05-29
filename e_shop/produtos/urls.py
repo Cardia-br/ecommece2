536c8cb72be82e005ca11c29400f1097dc8ddfca
Cardia-br <lucascardia16@gmail.com>
2025-05-29 17:20:16 -0300

    atualização de login e cadastro

--- e_shop/produtos/urls.py
+++ e_shop/produtos/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
 from .views import (
     ProdutoListView, ProdutoCreateView,
-    ProdutoUpdateView, ProdutoDeleteView
+    ProdutoUpdateView, ProdutoDeleteView,
+    LoginView, LogoutView, RegisterView
 )
 
 urlpatterns = [
@@ -10,4 +10,7 @@ urlpatterns = [
     path('adicionar/', ProdutoCreateView.as_view(), name='adicionar_produto'),
     path('editar/<int:pk>/', ProdutoUpdateView.as_view(), name='editar_produto'),
     path('remover/<int:pk>/', ProdutoDeleteView.as_view(), name='remover_produto'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- e_shop/produtos/views.py
+++ e_shop/produtos/views.py
@@ -2,32 +2,44 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Produto
 from .forms import ProdutoForm
+from django.contrib.auth.views import LoginView as AuthLoginView, LogoutView as AuthLogoutView
+from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-
-class ProdutoListView(ListView):
+class ProdutoListView(LoginRequiredMixin, ListView):
     model = Produto
     template_name = 'produtos/lista.html'
     context_object_name = 'produtos'
 
-
-
-class ProdutoCreateView(CreateView):
+class ProdutoCreateView(LoginRequiredMixin, CreateView):
     model = Produto
     form_class = ProdutoForm
     template_name = 'produtos/form.html'
     success_url = reverse_lazy('listar_produtos')
 
-
-
-class ProdutoUpdateView(UpdateView):
+class ProdutoUpdateView(LoginRequiredMixin, UpdateView):
     model = Produto
     form_class = ProdutoForm
     template_name = 'produtos/form.html'
     success_url = reverse_lazy('listar_produtos')
 
-
-class ProdutoDeleteView(DeleteView):
+class ProdutoDeleteView(LoginRequiredMixin, DeleteView):
     model = Produto
-    template_name = 'produtos/confirm_delete.html'  
+    template_name = 'produtos/confirm_delete.html'
     success_url = reverse_lazy('listar_produtos')
+
+
+# Tela de login
+class LoginView(AuthLoginView):
+    template_name = 'login.html'
+
+# Tela de logout
+class LogoutView(AuthLogoutView):
+    next_page = reverse_lazy('login')
+
+# Tela de cadastro
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('login')
